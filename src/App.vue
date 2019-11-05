52c7ef5f3a81a512cbb22ab73c1f1529400d21cb
Chris <chris.kieran@gmail.com>
2019-11-04 21:15:40 -0500

    basic app setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <Landing 
+        v-if="introClicked == false"  
+        @click="toggleIntro()"
+      ></Landing>
+      <Home v-else></Home>
+
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from './components/Landing.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Landing,
+    Home
+  },
+  data: function() {
+    return {
+    introClicked: false
+    }
+  },
+  methods: {
+    toggleIntro: function () {
+      this.introClicked == !this.introClicked;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.testClass {
+  border: 2px solid green;
 }
 </style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="header-container">
+        <header>
+            <a href="web.html">
+                <h1>chris kieran</h1>
+            </a>
+        </header>
+    </div> 
+    <h2><a href="web.html">modern web development</a></h2> 
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
