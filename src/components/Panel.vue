9c032db3a0624e9916e36a11e9cb320d3092990e
Chris <chris.kieran@gmail.com>
2020-01-14 21:36:50 -0500

    panel hover effect

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -4,11 +4,36 @@
 </template>
 
 <style lang="scss" scoped>
-    .panel {
-        width: 80px;
-        margin: 10px;
-        background-repeat: no-repeat;
+.panel {
+    width: 80px;
+    margin: 10px;
+    background-repeat: no-repeat;
+    border-radius: 5px;
+}
+.panel:hover {
+    animation: shake 5s cubic-bezier(.36,.07,.19,.97) both;
+    transform: translate3d(0, 0, 0);
+    backface-visibility: hidden;
+    perspective: 1000px;
     }
+
+@keyframes shake {
+    10%, 90% {
+        transform: translate3d(0, -1px, 0);
+    }
+    
+    20%, 80% {
+        transform: translate3d(0, 2px, 0);
+    }
+
+    30%, 50%, 70% {
+        transform: translate3d(0, -4px, 0);
+    }
+
+    40%, 60% {
+        transform: translate3d(0, 4px, 0);
+    }
+}
 
 @media screen and (max-width: 1200px) {
   .panel {
